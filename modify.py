5248d5c013244877960fa8952006c1c41eb2a636
Jonathan James Watson <23344719+jonathanjameswatson@users.noreply.github.com>
2021-12-07 16:22:33 +0000

    Add republish flag to script

--- modify.py
+++ modify.py
@@ -7,12 +7,28 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "--file", default="package.json", help="The package.json file to modify."
 )
+parser.add_argument(
+    "--republish",
+    action="store_true",
+    help="Whether other manifest options should be changed for republishing.",
+)
 args = parser.parse_args()
 
 with open(args.file, "r") as f:
     data = json.load(f)
 
 del data["contributes"]["grammars"]
+
+if args.republish:
+    data["name"] = "grammarless-markdown-all-in-one"
+    del data["icon"]
+    data["publisher"] = "jonathanjameswatson"
+    data["bugs"][
+        "url"
+    ] = "https://github.com/jonathanjameswatson/grammarless-vscode-markdown/issues"
+    data["repository"][
+        "url"
+    ] = "https://github.com/jonathanjameswatson/grammarless-vscode-markdown"
 
 with open(args.file, "w") as f:
     json.dump(data, f, indent=4)
